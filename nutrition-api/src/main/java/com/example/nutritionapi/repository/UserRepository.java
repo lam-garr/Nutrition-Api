8999dacb63b6014053acce2766ac9255824efe4d
Garrett <lamgarr@outlook.com>
2023-05-16 21:44:49 -0700

    Add user repository interface

---
+++ nutrition-api/src/main/java/com/example/nutritionapi/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.nutritionapi.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.nutritionapi.models.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    
+}
