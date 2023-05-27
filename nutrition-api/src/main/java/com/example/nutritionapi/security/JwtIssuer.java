41f590470ea3fa4c63b67c5539f284cb32cb8131
Garrett <lamgarr@outlook.com>
2023-05-26 21:58:16 -0700

    Add jwt issuer

---
+++ nutrition-api/src/main/java/com/example/nutritionapi/security/JwtIssuer.java
@@ -0,0 +1,21 @@
+package com.example.nutritionapi.security;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import org.springframework.stereotype.Component;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+@Component
+public class JwtIssuer {
+    public String issue(int userId, String username) {
+        return JWT.create()
+        .withSubject(String.valueOf(userId))
+        .withClaim("username", username)
+        .withExpiresAt(Instant.now().plus(Duration.of(1,ChronoUnit.DAYS)))
+        .sign(Algorithm.HMAC256("secret"));
+    }
+}
