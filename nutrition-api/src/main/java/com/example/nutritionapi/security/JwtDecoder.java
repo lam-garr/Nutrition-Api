2b1b2fc152962d33888d193b829f3e258162ad32
Garrett <lamgarr@outlook.com>
2023-05-28 18:25:30 -0700

    Add jwt decoder

---
+++ nutrition-api/src/main/java/com/example/nutritionapi/security/JwtDecoder.java
@@ -0,0 +1,16 @@
+package com.example.nutritionapi.security;
+
+import org.springframework.stereotype.Component;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+@Component
+public class JwtDecoder {
+    public DecodedJWT decode(String token) {
+        return JWT.require(Algorithm.HMAC256("secret"))
+            .build()
+            .verify(token);
+    }
+}
