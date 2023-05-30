4ea080f6f5cbca22b6211eabc2da0b129ddeec5e
Garrett <lamgarr@outlook.com>
2023-05-29 21:40:46 -0700

    Add UserPrincipal class

---
+++ nutrition-api/src/main/java/com/example/nutritionapi/security/UserPrincipal.java
@@ -0,0 +1,48 @@
+package com.example.nutritionapi.security;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UserPrincipal implements UserDetails{
+    private final int userId;
+    private final String username;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public String getPassword() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPassword'");
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
