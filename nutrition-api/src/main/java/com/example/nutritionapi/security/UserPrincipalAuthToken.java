715ee1e5e29c9568bc779e46c2dbae64409dd511
Garrett <lamgarr@outlook.com>
2023-05-30 18:35:45 -0700

    Add UserPrincipalAuthToken class

---
+++ nutrition-api/src/main/java/com/example/nutritionapi/security/UserPrincipalAuthToken.java
@@ -0,0 +1,23 @@
+package com.example.nutritionapi.security;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+
+public class UserPrincipalAuthToken extends AbstractAuthenticationToken{
+    private final UserPrincipal principal;
+
+    public UserPrincipalAuthToken(UserPrincipal principal) {
+        super(principal.getAuthorities());
+        this.principal = principal;
+        setAuthenticated(true);
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public UserPrincipal getPrincipal() {
+        return principal;
+    }
+}
