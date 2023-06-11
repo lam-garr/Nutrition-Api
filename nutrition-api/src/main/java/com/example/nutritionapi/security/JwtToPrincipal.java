8df8595acb42606fdc8abb0275259896b772f055
Garrett <lamgarr@outlook.com>
2023-06-10 20:16:59 -0700

    Add JwtAuthFilter class

---
+++ nutrition-api/src/main/java/com/example/nutritionapi/security/JwtToPrincipal.java
@@ -0,0 +1,15 @@
+package com.example.nutritionapi.security;
+
+import org.springframework.stereotype.Component;
+
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+@Component
+public class JwtToPrincipal {
+    public UserPrincipal convert(DecodedJWT jwt){
+        return UserPrincipal.builder()
+            .userId(Integer.valueOf(jwt.getSubject()))
+            .username(jwt.getClaim("username").asString())
+            .build();
+    }
+}
