9a34557e6872ad58994e0b90ab8560c435482c0d
Garrett <lamgarr@outlook.com>
2023-06-11 21:20:24 -0700

    Refactor dummy login controller

--- nutrition-api/src/main/java/com/example/nutritionapi/controllers/AuthController.java
+++ nutrition-api/src/main/java/com/example/nutritionapi/controllers/AuthController.java
@@ -1,21 +1,42 @@
 package com.example.nutritionapi.controllers;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.nutritionapi.models.LoginReq;
 import com.example.nutritionapi.models.LoginRes;
+import com.example.nutritionapi.security.JwtIssuer;
+import com.example.nutritionapi.security.UserPrincipal;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
-    @PostMapping("/login")
-    public LoginRes login(@RequestBody LoginReq req){
+
+    private final JwtIssuer jwtIssuer;
+
+    private final AuthenticationManager authenticationManager;
+    
+    @GetMapping("/login")
+    public LoginRes login(){
+        var authentication = authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken("Username", "password")
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        var principal = (UserPrincipal) authentication.getPrincipal();
+
+        var token = jwtIssuer.issue(principal.getUserId(), principal.getUsername());
+
         return LoginRes.builder()
-            .accessToken("accessToken")
+            .accessToken(token)
             .build();
     }
 }
