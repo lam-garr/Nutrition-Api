91a48b893c00aeabc85a7f9fec776ab5dc681815
Garrett <lamgarr@outlook.com>
2023-07-14 18:32:00 -0700

    Add DateReq class

---
+++ nutrition-api/src/main/java/com/example/nutritionapi/models/DateReq.java
@@ -0,0 +1,12 @@
+package com.example.nutritionapi.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DateReq {
+    private String month;
+    private int day;
+    private int year;
+}
