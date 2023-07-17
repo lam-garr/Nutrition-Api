5bcb0de11500231379aaac06b9c7ec1c1f344e91
Garrett <lamgarr@outlook.com>
2023-07-16 21:17:37 -0700

    Add SortCriteriaReq class

---
+++ nutrition-api/src/main/java/com/example/nutritionapi/models/SortCriteriaReq.java
@@ -0,0 +1,11 @@
+package com.example.nutritionapi.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SortCriteriaReq {
+    private String sortCriteriaOne;
+    private String sortCriteriaTwo;
+}
