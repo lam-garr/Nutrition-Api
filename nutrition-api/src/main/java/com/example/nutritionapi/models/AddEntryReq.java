f889e7dd910878f88d357440ad5e614d70861ca5
Garrett <lamgarr@outlook.com>
2023-08-06 21:57:50 -0700

    Add AddEntryReq class

---
+++ nutrition-api/src/main/java/com/example/nutritionapi/models/AddEntryReq.java
@@ -0,0 +1,11 @@
+package com.example.nutritionapi.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddEntryReq {
+    private String id;
+    private String day;
+}
